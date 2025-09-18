b352e2d005bf85ceedf3a07a4798880aa129ade3
Abarquez Yohann <yohann.abarquez@cit.edu>
2025-09-18 12:43:35 +0800

    change add function

--- src/boardarray.h
+++ src/boardarray.h
@@ -16,6 +16,7 @@ class BoardArray : public Board {
         void add(Entry* entry) {
             // TODO: IMPLEMENT THIS FUNCTION
             // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
+            return;
         }
 
         void print() {
